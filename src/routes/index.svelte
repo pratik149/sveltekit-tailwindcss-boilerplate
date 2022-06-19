2ec7f8588e6298e515a48accf0466bae8b7d6a09
Pratik Rane <pratikrane149@gmail.com>
2022-06-19 15:19:21 +0530

    chore: :mag: add seo tags

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,9 @@
-<div class="max-w-screen-md mx-auto text-center">
+<svelte:head>
+  <title>Welcome</title>
+  <meta name="description" content="Boilerplate for SvelteKit and TailwindCSS">
+</svelte:head>
+
+<main class="max-w-screen-md mx-auto text-center">
 	<h1 class="text-3xl font-bold text-cyan-900">Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</div>
+</main>
