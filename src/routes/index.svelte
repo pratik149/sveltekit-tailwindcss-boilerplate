bb92f1cb2c07dd7d213aa62609c88d8dad9e92b7
Pratik Rane <pratikrane149@gmail.com>
2022-06-19 14:39:19 +0530

    feat: :sparkles: install and configure `tailwindcss`

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="max-w-screen-md mx-auto text-center">
+	<h1 class="text-3xl font-bold text-cyan-900">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
